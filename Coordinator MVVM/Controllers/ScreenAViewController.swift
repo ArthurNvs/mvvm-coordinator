ee296d55debc78609efb058774343d0815dc5a59
Arthur Neves <arthurnvsm@gmail.com>
2022-05-11 17:41:40 -0300

    add new fields to ScreenAViewController

--- Coordinator MVVM/Controllers/ScreenAViewController.swift
+++ Coordinator MVVM/Controllers/ScreenAViewController.swift
@@ -2,10 +2,40 @@ import Foundation
 import UIKit
 
 class ScreenAViewController: UIViewController {
-    var viewModel: ScreenAViewModel
+    var viewModelFactory: ScreenAViewModel
+    
+    private lazy var button: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 220, height: 30))
+        button.center = view.center
+        button.backgroundColor = .systemBlue
+        button.setTitle("Enviar nome completo", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .line
+        textField.textAlignment = .left
+        textField.center = view.center
+        textField.placeholder = "Name"
+        return textField
+    }()
+    
+    private lazy var lastNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .line
+        textField.textAlignment = .left
+        textField.center = view.center
+        textField.placeholder = "Last name"
+        return textField
+    }()
     
     init(viewModel: ScreenAViewModel){
-        self.viewModel = viewModel
+        self.viewModelFactory = viewModel
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -18,18 +48,27 @@ class ScreenAViewController: UIViewController {
         view.backgroundColor = .white
         title = "Screen A"
         
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 220, height: 55))
         view.addSubview(button)
-        button.center = view.center
-        button.backgroundColor = .systemBlue
-        button.setTitle("Screen B", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        view.addSubview(nameTextField)
+        view.addSubview(lastNameTextField)
+        configureSubviewsConstraints()
     }
     
     @objc func didTapButton() {
-        viewModel.goToScreenB()
+        viewModelFactory.goToScreenB()
     }
-    
 }
 
+private extension ScreenAViewController {
+    func configureSubviewsConstraints() {
+        NSLayoutConstraint.activate([
+            self.lastNameTextField.leadingAnchor.constraint(equalTo: button.leadingAnchor),
+            self.lastNameTextField.trailingAnchor.constraint(equalTo: button.trailingAnchor),
+            self.lastNameTextField.bottomAnchor.constraint(equalTo: button.topAnchor),
+            
+            self.nameTextField.leadingAnchor.constraint(equalTo: lastNameTextField.leadingAnchor),
+            self.nameTextField.trailingAnchor.constraint(equalTo: lastNameTextField.trailingAnchor),
+            self.nameTextField.bottomAnchor.constraint(equalTo: lastNameTextField.topAnchor),
+        ])
+    }
+}
