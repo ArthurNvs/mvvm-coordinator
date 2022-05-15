82c33afba40439998663e419ac6e2056ba6f80d9
Arthur Neves <arthurnvsm@gmail.com>
2022-05-14 22:59:41 -0300

    refactor ScreenA to use UIStackView

--- Coordinator MVVM/Scene A/AViewController.swift
+++ Coordinator MVVM/Scene A/AViewController.swift
@@ -7,7 +7,6 @@ class AViewController: UIViewController {
     private lazy var button: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.center = view.center
         button.backgroundColor = .systemBlue
         button.setTitle("send full name", for: .normal)
         button.setTitleColor(.white, for: .normal)
@@ -21,7 +20,6 @@ class AViewController: UIViewController {
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.borderStyle = .line
         textField.textAlignment = .left
-        textField.center = view.center
         textField.attributedPlaceholder = NSAttributedString(string: "Name", attributes: [
             .foregroundColor: UIColor.systemBlue,
             .font: UIFont.boldSystemFont(ofSize: 17),
@@ -34,12 +32,25 @@ class AViewController: UIViewController {
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.borderStyle = .line
         textField.textAlignment = .left
-        textField.center = view.center
         textField.attributedPlaceholder = NSAttributedString(string: "Last Name", attributes: [
             .foregroundColor: UIColor.systemBlue,
             .font: UIFont.boldSystemFont(ofSize: 17)
         ])
         return textField
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = NSLayoutConstraint.Axis.vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        
+        stackView.addArrangedSubview(nameTextField)
+        stackView.addArrangedSubview(lastNameTextField)
+        stackView.addArrangedSubview(button)
+        return stackView
     }()
     
     init(viewModel: AViewModel){
@@ -56,9 +67,7 @@ class AViewController: UIViewController {
         view.backgroundColor = .white
         title = "Screen A"
         
-        view.addSubview(button)
-        view.addSubview(nameTextField)
-        view.addSubview(lastNameTextField)
+        view.addSubview(stackView)
         configureSubviewsConstraints()
     }
     
@@ -74,19 +83,22 @@ class AViewController: UIViewController {
 private extension AViewController {
     func configureSubviewsConstraints() {
         NSLayoutConstraint.activate([
-            self.lastNameTextField.leadingAnchor.constraint(equalTo: button.leadingAnchor),
-            self.lastNameTextField.trailingAnchor.constraint(equalTo: button.trailingAnchor),
-            self.lastNameTextField.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -10),
             
-            self.nameTextField.leadingAnchor.constraint(equalTo: lastNameTextField.leadingAnchor),
-            self.nameTextField.trailingAnchor.constraint(equalTo: lastNameTextField.trailingAnchor),
-            self.nameTextField.bottomAnchor.constraint(equalTo: lastNameTextField.topAnchor, constant: -10),
+            self.stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 250),
+            self.stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            self.stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            //self.stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            //self.stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            //self.stackView.heightAnchor.constraint(equalToConstant: 50),
             
-            self.button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            self.button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            self.button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            self.button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            self.button.heightAnchor.constraint(equalToConstant: 40)
+            self.lastNameTextField.heightAnchor.constraint(equalToConstant: 40),
+            self.lastNameTextField.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+            
+            self.nameTextField.heightAnchor.constraint(equalToConstant: 40),
+            self.nameTextField.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+            
+            self.button.heightAnchor.constraint(equalToConstant: 40),
+            self.button.widthAnchor.constraint(equalTo: stackView.widthAnchor)
         ])
     }
 }
